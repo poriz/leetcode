2ff94131a264488dcc37a34f56203ba5d69c177c
poriz <36216087+poriz@users.noreply.github.com>
2024-05-28 16:02:59 +0900

    Time: 2085 ms (12.79%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -1,7 +1,9 @@
-SELECT p.product_id, COALESCE(ROUND(SUM(p.price * u.units) / SUM(u.units),2),0) AS average_price
+SELECT p.product_id, 
+    COALESCE(ROUND(SUM(p.price * u.units) / SUM(u.units),2),0) AS average_price
 FROM Prices AS p
-LEFT JOIN UnitsSold AS u
-    ON p.product_id = u.product_id AND u.purchase_date BETWEEN p.start_date AND p.end_date
+    LEFT JOIN UnitsSold AS u
+    ON p.product_id = u.product_id 
+    AND u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id
 
 
