f6f4e6696532e8e74bfa0cfcc7798fecda7f30a5
poriz <36216087+poriz@users.noreply.github.com>
2024-06-03 11:33:40 +0900

    Time: 1017 ms (82.15%), Space: 0B (100%) - LeetHub

---
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,13 @@
+WITH Rs AS (
+SELECT
+    delivery_id,
+    customer_id,
+    order_date,
+    customer_pref_delivery_date,
+    ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date) AS rn
+FROM Delivery
+)
+SELECT
+    ROUND(SUM(CASE WHEN order_date=customer_pref_delivery_date THEN 1 ELSE 0 END) / COUNT(*) * 100,2) AS immediate_percentage
+FROM Rs
+WHERE rn = 1;
