f49c0af2b33225836c4d0af33e455b3317257dc9
poriz <36216087+poriz@users.noreply.github.com>
2024-06-10 11:52:42 +0900

    Time: 483 ms (98.77%), Space: 0B (100%) - LeetHub

---
+++ LeetCode/Medium/0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -0,0 +1,11 @@
+WITH temp AS (
+    SELECT id,num,
+    LEAD(id) OVER (ORDER BY id) AS id1,
+    LEAD(num) OVER (ORDER BY id) AS num1,
+    LEAD(id,2) OVER (ORDER BY id) AS id2,
+    LEAD(num,2) OVER (ORDER BY id) AS num2
+    FROM Logs
+)
+SELECT DISTINCT(num) AS ConsecutiveNums
+FROM temp
+WHERE (id1 - id) = 1 AND (id2 - id1) = 1 AND num = num1 AND num1 = num2
