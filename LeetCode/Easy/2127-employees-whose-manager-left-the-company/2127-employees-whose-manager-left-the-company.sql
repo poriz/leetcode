2c5068195d87da009b3d43a69c7ef0caf7120840
poriz <36216087+poriz@users.noreply.github.com>
2024-06-14 11:32:47 +0900

    Time: 369 ms (88.31%), Space: 0B (100%) - LeetHub

--- LeetCode/Easy/2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
+++ LeetCode/Easy/2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
@@ -1,9 +1,4 @@
-WITH temp AS (
-    SELECT *
-    FROM Employees
-    WHERE manager_id is not null AND salary < 30000
-)
 select employee_id
-FROM temp
-WHERE manager_id not in (select employee_id from Employees)
+FROM Employees
+WHERE salary < 30000 and manager_id not in (select employee_id from Employees)
 ORDER BY 1
