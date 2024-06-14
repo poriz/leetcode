badffa7449db9147c492fff8d441c4ba4aabd26a
poriz <36216087+poriz@users.noreply.github.com>
2024-06-14 11:58:24 +0900

    Time: 353 ms (90.59%), Space: 0B (100%) - LeetHub

---
+++ LeetCode/Medium/0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,22 @@
+WITH Odd AS (
+    SELECT id,student
+    FROM Seat
+    WHERE id % 2 = 1
+),
+Even AS (
+    SELECT id,student
+    FROM Seat
+    WHERE id % 2 = 0
+),
+cte AS (
+    SELECT o.id AS oddID, o.student AS oddName, e.id AS evenID, e.student AS evenName
+    FROM Odd o
+    JOIN Even e On e.id -1 = o.id
+) 
+SELECT oddId AS id, evenName AS student FROM cte
+UNION
+SELECT evenID, oddName FROM cte
+UNION
+SELECT id, student FROM Seat
+WHERE id NOT IN (SELECT oddID FROM cte UNION SELECT evenID FROM cte)
+ORDER BY id;
